827f45d4acfc13c00b5140b5a70783ce0cbfe5c0
Pragati Singh <pragatisingh0417@gmail.com>
2026-08-21 13:26:59 +0530

    updated by pragati

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,7 +4,7 @@ dotenv.config();
 const app = require("./app");
 const connectDB = require("../config/db");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // Connect to MongoDB then start server
 connectDB().then(() => {
